ccd40e41925989b6df4911ce34c91b6256fb7785
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-10-26 14:40:34 -0600

    Update pizza API to include Toppings query

--- packages/server/src/application/providers/pizzas/pizza.provider.types.ts
+++ packages/server/src/application/providers/pizzas/pizza.provider.types.ts
@@ -1,7 +1,10 @@
+import { Topping } from '../toppings/topping.provider.types';
+
 export interface Pizza {
   id: string;
   name: string;
   description: string;
   toppingIds: string[];
   imgSrc: string;
+  toppings: Topping[];
 }
